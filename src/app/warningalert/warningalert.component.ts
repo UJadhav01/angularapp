9c0d5acde85955c8ab07c40529ccc8633f867610
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-11-23 16:03:18 +0530

    warningalert.component.ts

---
+++ src/app/warningalert/warningalert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-warningalert',
+  template:`<h3>Created component successfully</h3>`,
+  styleUrls: ['./warningalert.component.css']
+})
+
+export class WarningAlertComponent{
+
+}

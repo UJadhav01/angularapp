789b23806a6d12e77908fe29b6a158b23ea21033
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-11-24 14:06:47 +0530

    server.component.ts

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,9 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
+name:string='Hello Champ , u are great learner';
+allowNewServer:boolean=false;
+imgUrl='https://images.pexels.com/photos/757889/pexels-photo-757889.jpeg'
 
-  constructor() { }
+serverCreationStatus:string='server not created'
+userName:string;
+serverName:string='server-1'
+  serverStatus: string='online';
+  constructor() {
+    setTimeout(()=> {
+      this.allowNewServer = true
+    }, 5000);
 
+  }
+  onServerCreated(){
+    this.serverCreationStatus='server created successfully'
+  }
+  getServerStatus() {
+    return this.serverStatus;
+  }
+  onClear(){
+    this.userName=null
+  }
   ngOnInit() {
   }
 

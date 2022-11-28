5d6db46be9143c9e5756653d219220c561367c73
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-11-28 21:31:32 +0530

    custome directive

---
+++ src/app/customdirective.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+import { AbstractControl, NgForm, NG_VALIDATORS, ValidationErrors, Validator} from '@angular/forms';
+
+@Directive({
+  selector: '[appCustomDirective]',
+
+})
+export class CustomDirectiveDirective {
+
+  constructor(private el:ElementRef<HTMLElement>,private renderer:Renderer2) {
+ console.log(`custom directive call`);
+ el.nativeElement.style.margin='30px 30px';
+ el.nativeElement.style.border='2px solid blue';
+ el.nativeElement.style.padding='10px';
+ el.nativeElement.style.color='#d122a3';
+ renderer.setStyle(el.nativeElement,'background','#dddd');//when want to run application other than browser always use renderer
+
+}
+}
+

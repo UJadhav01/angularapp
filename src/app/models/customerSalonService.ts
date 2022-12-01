fc486b50f3785f5648efe437828393bf29df4faa
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-12-01 17:57:39 +0530

    customerSalonService.ts

---
+++ src/app/models/customerSalonService.ts
@@ -0,0 +1,9 @@
+import { Time } from "@angular/common";
+
+export class CustomerSalonService{
+customerName:string;
+wantService:string;
+selectDate:Date;
+selectTime:Date;
+customerMobile:number;
+}

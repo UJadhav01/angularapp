cd81183736a4e7913127f6cab74657efe7fe46cd
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-12-01 22:49:41 +0530

    directive second assignment

---
+++ src/app/toggle-button/toggle-button.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle-button',
+  templateUrl: './toggle-button.component.html',
+  styleUrls: ['./toggle-button.component.css']
+})
+export class ToggleButtonComponent implements OnInit {
+
+  showSecretParagraph = false;
+  onButtonClicks:number[]=[];
+  count=1;
+
+  ngOnInit(): void {
+  }
+
+  onToggleParagraph() {
+    this.showSecretParagraph = !this.showSecretParagraph;
+    this.onButtonClicks.push(this.count);
+    this.count++;
+  }
+  getColor(onClick:number){
+    return onClick > 4 ? 'blue' : 'default';
+    }
+    onTextColor(onClick2:number){
+      return onClick2 > 4 ? 'white' : 'default';
+
+    }
+  }
+

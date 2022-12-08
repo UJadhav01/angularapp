d4a06c867c045b7ab6ea4f10ee375351f9042892
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-12-09 00:19:21 +0530

    template drivan form

---
+++ src/app/simple-form/simple-form.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-simple-form',
+  templateUrl: './simple-form.component.html',
+  styleUrls: ['./simple-form.component.css'],
+
+})
+export class SimpleFormComponent implements OnInit {
+  NgForm=NgForm;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  OnSubmit(form:NgForm){
+console.log(form);
+
+  }
+
+}

1e3588765880794f38622d45f18e4d3f98561989
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-12-09 00:18:48 +0530

    app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,40 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserModule} from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
+
+import { HeaderComponent } from './header/header.component';
+
 import { ServerComponent } from './server/server.component';
 import { SuccessalertComponent } from './successalert/successalert.component';
 import { WarningAlertComponent } from './warningalert/warningalert.component';
-import { HeaderComponent } from './header/header.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { DirectivedemoComponent } from './directivedemo/directivedemo.component';
-import { NgswitchComponent } from './ngswitch/ngswitch.component';
 import { StructureDirectiveComponent } from './structure-directive/structure-directive.component';
 import { AttributeDirectiveComponent } from './attribute-directive/attribute-directive.component';
+import { NgswitchComponent } from './ngswitch/ngswitch.component';
+
 import { ComparepassComponent } from './comparepass/comparepass.component';
 import { ComparepassDirective } from './comparepass.directive';
-import { DecoratorAssComponent } from './decorator-ass/decorator-ass.component';
-import { OutputDecoratorComponent } from './output-decorator/output-decorator.component';
-import { AngularHooksLifeCycleComponent } from './angular-hooks-life-cycle/angular-hooks-life-cycle.component';
-import { ParentHooksLifeCycleComponent } from './parent-hooks-life-cycle/parent-hooks-life-cycle.component';
+
 import { ToggleButtonComponent } from './toggle-button/toggle-button.component';
 import { AddRemoveContentOnButtonComponent } from './add-remove-content-on-button/add-remove-content-on-button.component';
+
+import { DecoratorAssComponent } from './decorator-ass/decorator-ass.component';
+import { OutputDecoratorComponent } from './output-decorator/output-decorator.component';
+
 import { HooksCycleComponent } from './hooks-cycle/hooks-cycle.component';
 import { HooksCycleChildComponent } from './hooks-cycle-child/hooks-cycle-child.component';
+
+import { AngularHooksLifeCycleComponent } from './angular-hooks-life-cycle/angular-hooks-life-cycle.component';
+import { ParentHooksLifeCycleComponent } from './parent-hooks-life-cycle/parent-hooks-life-cycle.component';
+
+import { SimpleFormComponent } from './simple-form/simple-form.component';
+import { StudentRegistrationFormComponent } from './student-registration-form/student-registration-form.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -43,9 +56,12 @@ import { HooksCycleChildComponent } from './hooks-cycle-child/hooks-cycle-child.
     AddRemoveContentOnButtonComponent,
     HooksCycleComponent,
     HooksCycleChildComponent,
+    SimpleFormComponent,
+    StudentRegistrationFormComponent,
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule

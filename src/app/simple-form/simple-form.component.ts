acc9f80419ff6ff7ba9fca8e5969aa9680a53a3b
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-12-09 21:11:03 +0530

    TDF

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -8,9 +8,8 @@ import { NgForm } from '@angular/forms';
 
 })
 export class SimpleFormComponent implements OnInit {
-  NgForm=NgForm;
   constructor() { }
-
+username;
   ngOnInit() {
   }
   OnSubmit(form:NgForm){

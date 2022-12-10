0642d3cdf8018e3a5a0e42bd6fe41490b3ecbb65
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-12-10 14:05:40 +0530

    simpleTDF

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+
+class UserData{
+  name:string;
+  email:string;
+  gender:string;
+  courses:string;
+}
+
 @Component({
   selector: 'app-simple-form',
   templateUrl: './simple-form.component.html',
@@ -9,12 +17,36 @@ import { NgForm } from '@angular/forms';
 })
 export class SimpleFormComponent implements OnInit {
   constructor() { }
-username;
+  defaultselected='Angular'
+  submitData=false;
+  userData=new UserData();
+  defaultGender:string='Male'
+  radioArray=[
+    {id:'1',value:'Male'},
+  {id:'2',value:'Female'},
+  {id:'3',value:'Other'}
+]
   ngOnInit() {
   }
   OnSubmit(form:NgForm){
+    this.submitData=true;
 console.log(form);
+this.userData.name=form.value.userDetails.username;
+this.userData.email=form.value.userDetails.email;
+this.userData.courses=form.value.course;
+this.userData.gender=form.value.gender;
+form.reset();
+form.controls['course'].setValue('Angular');
+form.control.get('course').setValue('Angular');
+form.control['gender'].setValue('Female');
+form.control.get('gender').setValue('Female');
+
+
+    // this.onSaveData(this.userData);
 
   }
+onSaveData(uData:UserData){
+console.log(uData);
 
+}
 }

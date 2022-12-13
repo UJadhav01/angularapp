e625a95b055016041c546b7f0a2b5ca1cd9df7a6
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-12-13 23:26:07 +0530

    reactive form component

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -10,6 +10,14 @@ export class ReactiveFormComponent implements OnInit {
 
   myReactiveForm:FormGroup;
 
+  radioArray=[
+    {id:'1',value:'Male'},
+  {id:'2',value:'Female'},
+  {id:'3',value:'Other'}
+]
+
+userNames:any = ['codemind', 'technology', 'codemind technology'];
+
   constructor() {
     this.onCreateForm();
 
@@ -17,14 +25,25 @@ export class ReactiveFormComponent implements OnInit {
 
   ngOnInit() {
   }
+  selectedValue="";
+
 onCreateForm(){
   this.myReactiveForm=new FormGroup({
-'userName':new FormControl(''),
-'email':new FormControl(''),
+'userName':new FormControl('',[Validators.required,this.forbiddenNameValidator.bind(this)]),
+'email':new FormControl('',[Validators.required,Validators.email]),
+'course':new FormControl('Angular'),
+'gender':new FormControl('Male')
   })}
 
   onSubmit(){
 console.log(this.myReactiveForm);
+  }
 
+
+  forbiddenNameValidator(control: FormControl){
+ if(this.userNames.indexOf(control.value) !==-1){
+  return {'userNameNotAvailable':true}
+ }
+ return null;
   }
 }

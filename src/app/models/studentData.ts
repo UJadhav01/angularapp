3956ab760ce6d4e9cd54b81dbb3b5466d58a5201
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-12-16 18:14:29 +0530

    studentData class

---
+++ src/app/models/studentData.ts
@@ -0,0 +1,12 @@
+export class StudentData{
+firstName:string;
+lastName:string;
+age:number;
+email:string;
+phone:number;
+city:string;
+gender:string;
+password:string;
+confirmPassword:string;
+
+}

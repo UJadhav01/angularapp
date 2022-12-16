5047ba63b16e74f23c2772461940f718030d9610
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-12-16 18:17:01 +0530

    5thassignment.component.ts

---
+++ src/app/tdf5thassignment/tdf5thassignment.component.ts
@@ -0,0 +1,74 @@
+import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { StudentData } from '../models/studentData';
+
+@Component({
+  selector: 'app-tdf5thassignment',
+  templateUrl: './tdf5thassignment.component.html',
+  styleUrls: ['./tdf5thassignment.component.css']
+})
+export class Tdf5thassignmentComponent implements OnInit {
+
+  //for name length checking purpose
+  firstName:string;
+  lastName:string;
+
+  //for age checking
+  age:number;
+
+  //for phone
+  phone:number;
+
+  //for city values
+  defaultSelectedCity='Mumbai'
+  city=['Mumbai','Kolkata','Delhi'];
+
+
+  defaultGender:string='Male'
+  radioArray=[
+    {id:'1',value:'Male'},
+  {id:'2',value:'Female'},
+  {id:'3',value:'Other'}
+]
+
+   //for password checking purpose
+   password:string;
+   confirmPassword:string;
+
+   submitted:boolean=false;
+
+   studentInformation;
+   userArray=[];
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  onStudRegistration(myForm:NgForm){
+    //showing errors below each input field if submitting without enter data
+    this.studentInformation=new StudentData();
+
+this.submitted=true;
+
+this.studentInformation.firstName=myForm.value.userDetails.firstName;
+  this.studentInformation.lastName=myForm.value.userDetails.lastName;
+  this.studentInformation.age=myForm.value.userDetails.age;
+  this.studentInformation.email=myForm.value.userDetails.email;
+  this.studentInformation.phone=myForm.value.userDetails.phone;
+  this.studentInformation.city=myForm.value.userDetails.city;
+  this.studentInformation.gender=myForm.value.userDetails.gender;
+  this.studentInformation.password=myForm.value.userDetails.password;
+  this.studentInformation.confirmPassword=myForm.value.userDetails.firstName;
+
+  this.userArray.push(this.studentInformation);
+
+
+//to show user data into console
+if(myForm.value !== ''){
+  console.log(myForm.value);
+}
+
+  }
+  }
+

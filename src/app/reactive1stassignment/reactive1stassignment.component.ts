5be0ab479eeeae491ae988127d85f7d6b5712f7b
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-12-16 18:15:55 +0530

    reactive.component.ts

---
+++ src/app/reactive1stassignment/reactive1stassignment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reactive1stassignment',
+  templateUrl: './reactive1stassignment.component.html',
+  styleUrls: ['./reactive1stassignment.component.css']
+})
+export class Reactive1stassignmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

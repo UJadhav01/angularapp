46c42d3a493a206ab6b3caede8a0ecf438f499eb
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-12-17 18:08:51 +0530

    css-notes.component

---
+++ src/app/css-notes/css-notes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-css-notes',
+  templateUrl: './css-notes.component.html',
+  styleUrls: ['./css-notes.component.css']
+})
+export class CssNotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

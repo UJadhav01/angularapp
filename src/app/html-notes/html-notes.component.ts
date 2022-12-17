aad666721a064ccb1a07a816eef1ea681fc4c4bb
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-12-17 18:10:53 +0530

    html-notes.component

---
+++ src/app/html-notes/html-notes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-html-notes',
+  templateUrl: './html-notes.component.html',
+  styleUrls: ['./html-notes.component.css']
+})
+export class HtmlNotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

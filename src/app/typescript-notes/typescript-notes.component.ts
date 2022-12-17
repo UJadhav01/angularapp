3a041e5d2ae931f27a4752e58576ba0270171f51
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-12-17 18:13:43 +0530

    typescript-notes.component

---
+++ src/app/typescript-notes/typescript-notes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-typescript-notes',
+  templateUrl: './typescript-notes.component.html',
+  styleUrls: ['./typescript-notes.component.css']
+})
+export class TypescriptNotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

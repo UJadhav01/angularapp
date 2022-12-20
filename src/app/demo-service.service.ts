1f8a76b5ca1e203b2850175bf03c48d880204a90
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-12-20 19:55:26 +0530

    demo-service

---
+++ src/app/demo-service.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DemoServiceService {
+  apiURL = 'https://jsonplaceholder.typicode.com/users';
+
+  constructor(private httpClient: HttpClient){}
+
+  movie=[
+    { title: '3 Idiots', director: 'Rajkumar', cast: 'Amir', releaseDate: '2007' },
+    { title: 'Kantara', director: 'Rishab', cast: 'Rishab', releaseDate: '2022'},
+    { title: 'KGF', director: 'RockBhai', cast:'Yash', releaseDate: '2019'}
+
+  ]
+
+
+  display(){
+    alert('Subscribe successfully');
+  }
+
+  getUsersData()
+  {
+   return this.httpClient.get(this.apiURL);
+  }
+
+}

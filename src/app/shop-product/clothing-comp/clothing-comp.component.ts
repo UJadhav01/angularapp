3042e0988a499d013287ff1acb65ed2397358a29
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-12-26 17:53:14 +0530

    clothing.component

---
+++ src/app/shop-product/clothing-comp/clothing-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clothing-comp',
+  templateUrl: './clothing-comp.component.html',
+  styleUrls: ['./clothing-comp.component.css']
+})
+export class ClothingCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

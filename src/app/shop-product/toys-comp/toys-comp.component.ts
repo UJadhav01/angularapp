f0b5a0081ba5f03ade89780f041aac2b59374892
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-12-26 17:54:19 +0530

    toy-component

---
+++ src/app/shop-product/toys-comp/toys-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toys-comp',
+  templateUrl: './toys-comp.component.html',
+  styleUrls: ['./toys-comp.component.css']
+})
+export class ToysCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

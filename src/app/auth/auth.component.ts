64a94084198c195fee0b52b9fa9dc62fc2a02a62
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-12-30 18:58:25 +0530

    auth.component.ts

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,7 @@
+import { NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _authService:AuthService,private _router:Router) { }
 
   ngOnInit() {
   }
+  onSubmit(form:NgForm){
+    if(!form.valid){
+      return;
+    }
+  }
+  onCheckLogin(userEmail,pass){
+var result=this._authService.isUserLoggedIn(userEmail,pass);
+if(result==true){
+  window.alert('Logging successfully');
+  this._router.navigate(['/shop-comp']);
+}else{
+  window.alert('please enter valid email and password');
 
+}
+  }
 }

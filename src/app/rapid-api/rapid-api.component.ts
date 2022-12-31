b02a132724b32c814923d934580cdf703135dc98
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2023-01-01 00:15:37 +0530

    rapid-api.component

---
+++ src/app/rapid-api/rapid-api.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rapid-api',
+  templateUrl: './rapid-api.component.html',
+  styleUrls: ['./rapid-api.component.css']
+})
+export class RapidApiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

8c5d60aa0317135381b92c6c250ae39a571ab6ca
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2023-01-03 13:17:53 +0530

    rxjs operators

---
+++ src/app/rxjs-component/rxjs-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rxjs-component',
+  templateUrl: './rxjs-component.component.html',
+  styleUrls: ['./rxjs-component.component.css']
+})
+export class RxjsComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

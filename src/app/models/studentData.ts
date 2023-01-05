74625949dcd96eef7dcb7f51215657a3d266723b
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2023-01-05 12:26:58 +0530

    student class

--- src/app/models/studentData.ts
+++ src/app/models/studentData.ts
@@ -8,17 +8,6 @@ public city:string;
 public gender:string;
 public password:string;
 public confirmPassword:string;
-constructor(firstName:string,lastName:string,age:number,email:string,phone:number,city:string,gender:string,
-  password:string, confirmPassword:string){
-this.firstName=firstName;
-this.lastName=lastName;
-this.age=age;
-this.email=email;
-this.phone=phone;
-this.city=city;
-this.gender=gender;
-this.password=password;
-this.confirmPassword=confirmPassword;
-}
+
 
 }
---
+++ src/app/models/studentData2.ts
@@ -0,0 +1,24 @@
+export class StudentData2{
+public firstName:string;
+public lastName:string;
+public age:number;
+public email:string;
+public phone:number;
+public city:string;
+public gender:string;
+public password:string;
+public confirmPassword:string;
+constructor(firstName:string,lastName:string,age:number,email:string,phone:number,city:string,gender:string,
+  password:string, confirmPassword:string){
+this.firstName=firstName;
+this.lastName=lastName;
+this.age=age;
+this.email=email;
+this.phone=phone;
+this.city=city;
+this.gender=gender;
+this.password=password;
+this.confirmPassword=confirmPassword;
+}
+
+}
